a3944d7470dbe546a478f1c6ff490de90cdbdab7
PMS <ms147850@gmail.com>
2023-03-18 16:34:41 +0900

    Initial commit

---
+++ src/main/java/com/osb/osbserverapp/OsbServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.osb.osbserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsbServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsbServerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osb/osbserverapp/OsbServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osb.osbserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsbServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
