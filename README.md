6c41bcda7959609612cab64942f15cb69d966be5
PMS <ms147850@gmail.com>
2023-03-19 22:03:22 +0900

    feat: README.md 추가

---
+++ README.md
@@ -0,0 +1,23 @@
+# OSB 검색서버
+
+### 1. 기본기능
+
+a. 블로그 검색 기능
+
+b. 인기 검색어 TOP 10 조회 기능
+
+
+### 2. 추가 기능 라이브러리 항목
+1. Swagger2 : RestfulAPI 명세서 제공 및 테스트 환경 제공 
+2. QueryDSL : 동적 쿼리 제공 및 컴파일 단계에서 문법오류 확인
+
+
+### 3. Config / Advice 항목
+[1. JpaConfig](/osb-server-app/src/main/java/com/osb/osbserverapp/config/JpaConfig.java) : QueryDSL 라이브러리 사용을 위한 Config 설정
+
+[2. SwaggerConfig](/osb-server-app/src/main/java/com/osb/osbserverapp/config/SwaggerConfig.java) : Swagger 환경 구축을 위한 Config
+
+[3. RestTemplateConfig](/osb-server-app/src/main/java/com/osb/osbserverapp/externalapi/config/RestTemplateConfig.java) : RestTemplate 사용 시 Connection timeout 및 최대 연결 세션 설정. @LoadBalanced 사용
+
+[4. ApiCommonAdvice](/osb-server-app/src/main/java/com/osb/osbserverapp/common/advice/ApiCommonAdvice.java) : Exception 처리 Advice
+
--- src/main/java/com/osb/osbserverapp/externalapi/config/RestTemplateConfig.java
+++ src/main/java/com/osb/osbserverapp/externalapi/config/RestTemplateConfig.java
@@ -16,10 +16,12 @@ public class RestTemplateConfig { // RestTemplate Config
     @LoadBalanced
     public RestTemplate getCustomRestTemplate() {
         HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
-        httpRequestFactory.setConnectionRequestTimeout(2000); // 2sec
-        httpRequestFactory.setReadTimeout(10000); // 10sec
-        HttpClient httpClient = HttpClientBuilder.create().setMaxConnTotal(200)
-                .setMaxConnPerRoute(20).build();
+        httpRequestFactory.setConnectionRequestTimeout(2000); // 연결요청초과시간 2sec
+        httpRequestFactory.setReadTimeout(10000); // 읽기시간초과 10sec
+        HttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(200) // 연결을 유지할 최대 숫자
+                .setMaxConnPerRoute(20) // 특정 경로당 최대 숫자
+                .build();
         httpRequestFactory.setHttpClient(httpClient);
         return new RestTemplate(httpRequestFactory);
     }
